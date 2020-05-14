1529799a2fd3ad0a8e75b5c45e29d71f9a6805d3
MachineGun3008 <nbcanhkhoa@gmail.com>
2020-05-14 11:22:28 +0700

    Repair Boot sector

--- KMT/KMTSystem/BootSector.h
+++ KMT/KMTSystem/BootSector.h
@@ -16,7 +16,7 @@ private:
 	uint32_t Sector_Per_Cluster;
 	uint64_t Total_Sector;
 	uint32_t Sector_Begin_Of_CM;
-	uint32_t Cluster_Begin_RDET;
+	uint32_t Sector_Begin_RDET;
 	const string cr = "Copyright belong to Anh Khoa vs Cong Minh vs Thanh Tu";
 	const uint16_t End_Marker = 43605;
 };
