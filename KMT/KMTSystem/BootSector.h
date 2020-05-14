654d65e61933246062422388d3622bac623ee1e8
MrSooo <51949526+MrSooo@users.noreply.github.com>
2020-05-14 23:18:11 +0700

    code bootsector

--- KMT/KMTSystem/BootSector.h
+++ KMT/KMTSystem/BootSector.h
@@ -6,19 +6,15 @@
 class BootSector
 {
 public:
-	BootSector();
-	void readBootSector();
-	void writeBootSector();
-	~BootSector();
+	void createBootSector(uint64_t size_of_vol);
 private:
 	const string label = " AKCMTT ";
 	uint32_t Byte_Per_Sector;
 	uint32_t Sector_Per_Cluster;
 	uint64_t Total_Sector;
 	uint32_t Sector_Begin_Of_CM;
-	uint32_t Sector_Begin_RDET;
+	uint32_t Cluster_Begin_RDET;
 	const string cr = "Copyright belong to Anh Khoa vs Cong Minh vs Thanh Tu";
 	const uint16_t End_Marker = 43605;
 };
 #endif // !_BOOTSECTOR_
-
